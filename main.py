41a01994f4c0f75791b1a8fb44407fe0d7484054
Lersj <larsbliksvrhansen@gmail.com>
2025-06-11 11:19:07 +0200

    Added player.py and circleshape.py to drawthe player on the screen. Updated main.py with the player class.

--- main.py
+++ main.py
@@ -4,9 +4,10 @@
 import pygame
 
 from constants import *
+from player import *
 
 def main():
-    pygame.init()
+    pygame.init() # Initialize the pygame library
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -14,14 +15,27 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()  # Set the frame rate with this after every loop
+    dt = 0  # dt = delta time, the time since the last frame
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    # Main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill("black")
-        pygame.display.flip()
-        
 
+        screen.fill("black")
+
+        # Update game objects here
+        player.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000 # Manages fps and saves dt in seconds.
+        # THIS NEEDS TO BE AT THE END OF THE LOOP
 if __name__ == "__main__":
     main()
